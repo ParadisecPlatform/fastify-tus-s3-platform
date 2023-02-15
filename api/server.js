3b4af873c2a5709fcccb641dfb5e41c3b810628c
Marco La Rosa <m@lr.id.au>
2023-02-15 15:01:05 +1100

    rename to server.js and wire in tus uploader as a plugin

---
+++ api/server.js
@@ -0,0 +1,39 @@
+import Fastify from "fastify";
+import fastifyCompress from "@fastify/compress";
+import fastifySensible from "@fastify/sensible";
+const envToLogger = {
+    development: {
+        transport: {
+            target: "@fastify/one-line-logger",
+        },
+    },
+};
+const fastify = Fastify({ logger: envToLogger[process.env.NODE_ENV] });
+import tusS3Uploader from "./src/index.js";
+
+main();
+async function main() {
+    fastify.addHook("onReady", () => {
+        console.log();
+        console.log("Registered routes:");
+        routes.map((r) => console.log(`${r.method}: ${r.url}`));
+        console.log("");
+    });
+    const routes = [];
+    fastify.addHook("onRoute", (route) => {
+        routes.push(route);
+    });
+    fastify.register(fastifySensible);
+    fastify.register(fastifyCompress);
+    fastify.register(tusS3Uploader, {
+        awsAccessKeyId: "root",
+        awsSecretAccessKey: "rootpass",
+        endpoint: "http://minio:9000",
+        forcePathStyle: true,
+    });
+    fastify.addHook("onRequest", async (req, res) => {
+        // console.log("***", req.method, req.url);
+    });
+
+    fastify.listen({ port: 8080, host: "0.0.0.0" }, function (err, address) {});
+}
