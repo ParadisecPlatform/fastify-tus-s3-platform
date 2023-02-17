80539d2bb521a73e1326e0d9692ccd27e30b513a
Marco La Rosa <m@lr.id.au>
2023-02-17 17:11:26 +1100

    add configuration

--- api/src/config.js
+++ api/src/config.js
@@ -1,5 +1,14 @@
 // 500GB max file size
-export const maxFileSize = "536870912000";
+export const maximumFileSize = 500 * 1024 * 1024;
+
+// minimum part size = 5MB
+export const minimumPartSize = 5 * 1024 * 1024;
+
+// preferred part size = 8MB
+export const preferredPartSize = 8 * 1024 * 1024;
+
+// maximum number of parts
+export const maximumParts = 10000;
 
 // Supported TUS extensions
-export const tusExtensions = ["creation,creation-with-upload"];
+export const tusExtensions = ["creation"];
