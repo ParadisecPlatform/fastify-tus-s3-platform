4308e48934b40ecb660dbd537d60ab77e0356ca9
Marco La Rosa <m@lr.id.au>
2023-02-17 17:13:23 +1100

    config option rename

--- api/src/tus-options-handler.js
+++ api/src/tus-options-handler.js
@@ -1,10 +1,10 @@
-import { maxFileSize, tusExtensions } from "./config.js";
+import { maximumFileSize, tusExtensions } from "./config.js";
 export async function tusOptionsHandler(req, res) {
     res.code(204)
         .headers({
             "Tus-Resumable": "1.0.0",
             "Tus-Version": "1.0.0",
-            "Tus-Max-Size": maxFileSize,
+            "Tus-Max-Size": maximumFileSize,
             "Tus-Extension": tusExtensions,
         })
         .send();
