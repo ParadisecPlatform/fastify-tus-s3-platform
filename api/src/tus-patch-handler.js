4bd181a260d8b1c0785953b4a476bf24e1abd17c
Marco La Rosa <m@lr.id.au>
2023-02-17 17:22:05 +1100

    implement patch handler and tests

--- api/src/tus-patch-handler.js
+++ api/src/tus-patch-handler.js
@@ -1,92 +1,183 @@
-import { keyExists, bucketExists, createUpload, uploadPart, completeUpload } from "./s3-utils.js";
 import { Readable } from "stream";
-const MB = 1024 * 1024;
-const minimumFileSize = 5 * MB;
+import debug from "debug";
+import path from "node:path";
+import fsExtraPkg from "fs-extra";
+const {
+    stat,
+    open,
+    close,
+    write: nodeWrite,
+    readFile,
+    remove,
+    pathExists,
+    createReadStream,
+} = fsExtraPkg;
+import { minimumPartSize, preferredPartSize, maximumParts } from "./config.js";
+const log = debug("tus-s3-uploader:PATCH");
 
 export async function tusPatchHandler(req, res) {
-    console.log("");
-    console.log("");
-    console.log("");
-    console.log("start request");
-    console.log("");
+    log("start request");
     const uploadId = req.params.uploadId;
+    log("Upload Id", uploadId);
     let uploadData = await this.cache.get(uploadId);
-    if (!uploadData) return res.notFound();
 
-    console.log("uploadData", uploadData);
+    // if we don't find upload data for uploadId - fail out
+    if (!uploadData) return res.notFound();
+    // log("upload data", uploadData);
 
     // check that all of the expected headers are set correctly
+    const result = checkRequest(req, res);
+    if (result) return;
+
+    // const parts = Object.keys(uploadData.byPartNumber).map((part) => uploadData.byPartNumber[part]);
+    const contentLength = parseInt(req.headers["content-length"]);
+    const uploadOffset = parseInt(req.headers["upload-offset"]);
+    const fileSize = uploadData.fileSize;
+    const metadata = uploadData.metadata;
+    const optimalPartSize = calculateOptimalPartSize(fileSize);
+
+    log("File Size", fileSize);
+    log("Optimal part size for upload to S3", optimalPartSize);
+    log("Upload Offset", uploadOffset);
+    log("Content Length", contentLength);
+
+    const cacheFile = path.join(this.cache.basePath, uploadId);
+    log("Saving the chunk to the cache file");
+    let fd = await open(cacheFile, "a+");
+    await nodeWrite(fd, req.body);
+    await close(fd);
+    uploadData.bytesUploadedToServer += contentLength;
+
+    let cacheFileSize = (await stat(cacheFile)).size;
+
+    /**
+     * Once the cache file has reached the optimalPartSize
+     *  upload that part to s3. Note that S3 requires each part
+     *  to be the same size so only that amount is uploaded and
+     *  the remainder is left in the cacheFile until the buffer
+     *  is full again.
+     */
+    if (cacheFileSize >= optimalPartSize && uploadData.bytesUploadedToServer !== fileSize) {
+        log("uploading part to S3");
+        const parts = uploadData.parts;
+        const partNumber = parts.slice(-1).length ? parts.slice(-1)[0].PartNumber + 1 : 1;
+        let stream = createReadStream(cacheFile, { start: 0, end: optimalPartSize });
+        const part = await uploadPart.bind(this)({
+            metadata,
+            uploadId,
+            partNumber,
+            stream,
+        });
+        log("part uploaded to S3");
+        uploadData.parts.push(part);
+
+        // remove the uploaded bit from the cache file so that it's ready
+        //  for more to be added via subsequent PATCH requests
+
+        // read the cacheFile content into memory
+        let fd = await open(cacheFile, "r");
+        let data = await readFile(fd);
+        await close(fd);
+
+        // remove the file
+        await remove(cacheFile);
+
+        // write out the leftover from optimalPartSize to the end
+        // (that is, the first bit up to optimalPartSize is thrown away
+        //  and the rest is written back into the cachefile for next time)
+        fd = await open(cacheFile, "w+");
+        await nodeWrite(fd, data.subarray(optimalPartSize));
+        await close(fd);
+        log("upload data", uploadData);
+    }
+
+    /**
+     * When we've received all of the file data, upload the final part.
+     *  S3 allows this to be smaller than the other parts.
+     */
+    if (uploadData.bytesUploadedToServer === fileSize) {
+        log("uploading part to S3");
+        const parts = uploadData.parts;
+        const partNumber = parts.slice(-1).length ? parts.slice(-1)[0].PartNumber + 1 : 1;
+        let stream = createReadStream(cacheFile, { start: 0 });
+        const part = await uploadPart.bind(this)({
+            metadata,
+            uploadId,
+            partNumber,
+            stream,
+        });
+        log("part uploaded to S3");
+        uploadData.parts.push(part);
+        log("upload data", uploadData);
+    }
+    await this.cache.set(uploadId, uploadData);
+
+    let headers = {
+        "Tus-Resumable": "1.0.0",
+        "upload-offset": uploadData.bytesUploadedToServer,
+    };
+
+    if (uploadOffset + contentLength === fileSize) {
+        // we have the full file so complete the upload in S3
+        log("Complete the multipart upload to S3");
+        const parts = uploadData.parts;
+        await completeUpload.bind(this)({ metadata, uploadId, parts });
+        await remove(cacheFile);
+        await this.cache.remove(uploadId);
+
+        headers["upload-offset"] = fileSize;
+    }
+    res.code(204).headers(headers).send();
+    log("");
+}
+
+function checkRequest(req, res) {
     if (!req.body) return res.badRequest(`No data sent with patch request.`);
     if (!req.headers["content-length"])
         return res.badRequest(`'content-length' header is not set.`);
     if (!req.headers["upload-offset"]) return res.badRequest(`'upload-offset' header is not set.`);
     if (!req.headers["content-type"]) return res.badRequest(`'content-type' header is not set.`);
     if (req.headers["content-type"] !== "application/offset+octet-stream")
-        res.unsupportedMediaType(
+        return res.unsupportedMediaType(
             `'content-type' header must be 'application/offset+octet-stream'.`
         );
+}
 
-    const uploadOffset = req.headers["upload-offset"];
-    let partNumber;
-    if (uploadData?.byUploadOffset?.[uploadOffset]) {
-        // this is a part retry
-        let part = uploadData.byUploadOffset[uploadOffset];
-        console.log("retried part", part);
-        partNumber = part.partNumber;
-    } else {
-        // this is a new chunk
-        const contentLength = parseInt(req.headers["content-length"]);
-        uploadData.latestUploadOffset = uploadData.latestUploadOffset + contentLength;
-        uploadData.latestPartNumber = uploadData.latestPartNumber + 1;
-        console.log("new part", uploadData);
-        partNumber = uploadData.latestPartNumber;
-    }
-
-    const stream = Readable.from(req.body);
-    let part = await uploadPart({
-        client: this.s3client,
-        Bucket: uploadData.metadata.bucket,
-        Key: uploadData.metadata.filename,
+async function uploadPart({ metadata, uploadId, partNumber, stream }) {
+    let part = await this.storage.uploadPart({
+        Bucket: metadata.bucket,
+        Key: metadata.filename,
         uploadId,
         partNumber,
-        stream: stream.read(),
+        stream,
     });
-    uploadData = {
-        ...uploadData,
-        byUploadOffset: {
-            ...uploadData.byUploadOffset,
-            [req.headers["upload-offset"]]: part,
-        },
-        byPartNumber: {
-            ...uploadData.byPartNumber,
-            [partNumber]: part,
-        },
-    };
-    console.log(uploadData);
-    if (uploadData.uploadLength === uploadData.latestUploadOffset) {
-        // we've got the whole file, complete it
-        let parts = Object.keys(uploadData.byPartNumber).map(
-            (part) => uploadData.byPartNumber[part]
-        );
-        await completeUpload({
-            client: this.s3client,
-            Bucket: uploadData.metadata.bucket,
-            Key: uploadData.metadata.filename,
-            uploadId,
-            parts,
-        });
+    return part;
+}
+
+async function completeUpload({ metadata, uploadId, parts }) {
+    await this.storage.completeUpload({
+        Bucket: metadata.bucket,
+        Key: metadata.filename,
+        uploadId,
+        parts,
+    });
+}
+
+function calculateOptimalPartSize(size) {
+    let optimalPartSize;
+
+    // When upload is smaller or equal to PreferredPartSize, we upload in just one part.
+    if (size <= preferredPartSize) {
+        optimalPartSize = size;
+    }
+    // Does the upload fit in MaxMultipartParts parts or less with PreferredPartSize.
+    else if (size <= preferredPartSize * maximumParts) {
+        optimalPartSize = preferredPartSize;
+        // The upload is too big for the preferred size.
+        // We devide the size with the max amount of parts and round it up.
     } else {
-        await this.cache.set(uploadId, uploadData);
+        optimalPartSize = Math.ceil(size / maximumParts);
     }
 
-    const headers = {
-        "Tus-Resumable": "1.0.0",
-        "upload-offset": uploadData.latestUploadOffset,
-    };
-    console.log("");
-    console.log("end request");
-    console.log("");
-    console.log("");
-    console.log("");
-    res.code(204).headers(headers).send();
+    return optimalPartSize;
 }
--- api/src/tus-patch-handler.spec.js
+++ api/src/tus-patch-handler.spec.js
@@ -1,11 +1,13 @@
 import fetch from "cross-fetch";
-import { stat } from "node:fs/promises";
 import { createReadStream } from "node:fs";
-import { getS3Handle, keyExists, removeObjects, abortUpload } from "./s3-utils";
+import { Readable } from "node:stream";
+import { Storage } from "./s3-utils";
+import fsExtraPkg from "fs-extra";
+const { stat, open, close, write, remove, readFile, pathExists } = fsExtraPkg;
 
 describe.only(`Test TUS PATCH handling`, () => {
     const Bucket = "repository";
-    let s3client = getS3Handle({
+    let storage = new Storage({
         awsAccessKeyId: "root",
         awsSecretAccessKey: "rootpass",
         forcePathStyle: true,
@@ -17,7 +19,7 @@ describe.only(`Test TUS PATCH handling`, () => {
         });
         expect(response.status).toEqual(404);
     });
-    it(`Should succeed in performing a a PATCH request with data`, async () => {
+    it(`Should fail to perform a patch request as required headers not provided`, async () => {
         const file = "./src/config.js";
         const filename = new Buffer.from("config.js").toString("base64");
         const bucket = new Buffer.from("repository").toString("base64");
@@ -38,42 +40,256 @@ describe.only(`Test TUS PATCH handling`, () => {
         const uploadId = location.split("/").pop();
         let uploadOffset = response.headers.get("upload-offset");
 
+        //  no content-type header - expect 415
         let stream = createReadStream(file, { start: 0, end: 100 });
         response = await fetch(location, {
             method: "PATCH",
             headers: {
-                "content-type": "application/offset+octet-stream",
                 "content-length": 100,
                 "upload-offset": uploadOffset,
                 "tus-resumable": "1.0.0",
             },
             body: stream,
         });
-        expect(response.status).toEqual(204);
-        expect(response.headers.get("upload-offset")).toEqual(String(100));
+        expect(response.status).toEqual(415);
 
-        uploadOffset = response.headers.get("upload-offset");
-        stream = createReadStream(file, { start: 100, end: fileStats.size });
+        // content-type header with unsupported type - expect 415
+        stream = createReadStream(file, { start: 0, end: 100 });
         response = await fetch(location, {
             method: "PATCH",
             headers: {
-                "content-type": "application/offset+octet-stream",
-                "content-length": fileStats.size - 100,
+                "content-type": "application/octet-stream",
+                "content-length": 100,
                 "upload-offset": uploadOffset,
                 "tus-resumable": "1.0.0",
             },
             body: stream,
         });
-        expect(response.status).toEqual(204);
-        console.log(response.headers);
+        expect(response.status).toEqual(415);
 
-        // let exists = await keyExists({ client: s3client, Bucket, Key: "config.js" });
-        // expect(exists).toBeTrue;
+        // content length not defined - expect 400
+        stream = createReadStream(file, { start: 0, end: 100 });
+        response = await fetch(location, {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/offset+octet-stream",
+                "upload-offset": uploadOffset,
+                "tus-resumable": "1.0.0",
+            },
+            body: stream,
+        });
+        expect(response.status).toEqual(400);
 
-        await removeObjects({ client: s3client, Bucket: "repository", keys: ["config.js"] });
+        // upload-offset not defined - expect 400
+        stream = createReadStream(file, { start: 0, end: 100 });
+        response = await fetch(location, {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/offset+octet-stream",
+                "content-length": 100,
+                "tus-resumable": "1.0.0",
+            },
+            body: stream,
+        });
+        expect(response.status).toEqual(400);
+
+        await storage.abortUpload({ Bucket, Key: "config.js", uploadId });
     });
-    it.only("should skip this test", async () => {
-        console.warn("some requirement is missing");
-        return;
+    it(`Should succeed in performing a PATCH request with a very small file`, async () => {
+        const file = "./src/config.js";
+        const filename = "config.js";
+        const fileSize = (await stat(file)).size;
+        await uploadFile({ file, filename, chunkSize: 100 });
+
+        let exists = await storage.keyExists({ Bucket, Key: filename });
+        expect(exists).toBeTrue;
+
+        let s3FileStat = await storage.stat({ Bucket, Key: filename });
+        expect(s3FileStat.ContentLength).toEqual(fileSize);
+
+        let uploadedFile = await storage.downloadFile({
+            Bucket: "repository",
+            Key: filename,
+        });
+        let originalFile = await readFile(file);
+        expect(uploadedFile).toEqual(originalFile.toString());
+
+        await storage.removeObjects({
+            Bucket: "repository",
+            keys: [filename],
+        });
+    });
+    it(`Should succeed in uploading a 1MB file`, async () => {
+        const file = "./test-files/1mb.txt";
+        if (!(await pathExists(file))) {
+            console.log(
+                `Create this test file to run this test: 'dd if=/dev/zero of=api/test-files/1mb.txt bs=1024 count=1000'`
+            );
+            return;
+        }
+        const filename = "1mb.txt";
+        const fileSize = (await stat(file)).size;
+        await uploadFile({ file, filename, chunkSize: 100 * 1024 });
+
+        let exists = await storage.keyExists({ Bucket, Key: filename });
+        expect(exists).toBeTrue;
+
+        let s3FileStat = await storage.stat({ Bucket, Key: filename });
+        expect(s3FileStat.ContentLength).toEqual(fileSize);
+
+        let uploadedFile = await storage.downloadFile({
+            Bucket: "repository",
+            Key: filename,
+        });
+        let originalFile = await readFile(file);
+        expect(uploadedFile).toEqual(originalFile.toString());
+
+        await storage.removeObjects({
+            Bucket: "repository",
+            keys: [filename],
+        });
+    });
+    it(`Should succeed in uploading a 10MB file`, async () => {
+        const file = "./test-files/10mb.txt";
+        if (!(await pathExists(file))) {
+            console.log(
+                `Create this test file to run this test: 'dd if=/dev/zero of=api/test-files/10mb.txt bs=1024 count=10000'`
+            );
+            return;
+        }
+        const filename = "10mb.txt";
+        const fileSize = (await stat(file)).size;
+        await uploadFile({ file, filename, chunkSize: 4 * 1024 * 1024 });
+
+        let exists = await storage.keyExists({ Bucket, Key: filename });
+        expect(exists).toBeTrue;
+
+        let s3FileStat = await storage.stat({ Bucket, Key: filename });
+        expect(s3FileStat.ContentLength).toEqual(fileSize);
+
+        let uploadedFile = await storage.downloadFile({
+            Bucket: "repository",
+            Key: filename,
+        });
+        let originalFile = await readFile(file);
+        expect(uploadedFile).toEqual(originalFile.toString());
+
+        await storage.removeObjects({
+            Bucket: "repository",
+            keys: [filename],
+        });
+    });
+    it(`Should succeed in uploading a 50MB file`, async () => {
+        const file = "./test-files/50mb.txt";
+        if (!(await pathExists(file))) {
+            console.log(
+                `Create this test file to run this test: 'dd if=/dev/zero of=api/test-files/50mb.txt bs=1024 count=50000'`
+            );
+            return;
+        }
+        const filename = "50mb.txt";
+        const fileSize = (await stat(file)).size;
+        await uploadFile({ file, filename, chunkSize: 8 * 1024 * 1024 });
+
+        let exists = await storage.keyExists({ Bucket, Key: filename });
+        expect(exists).toBeTrue;
+
+        let s3FileStat = await storage.stat({ Bucket, Key: filename });
+        expect(s3FileStat.ContentLength).toEqual(fileSize);
+
+        let uploadedFile = await storage.downloadFile({
+            Bucket: "repository",
+            Key: filename,
+        });
+        let originalFile = await readFile(file);
+        expect(uploadedFile).toEqual(originalFile.toString());
+
+        await storage.removeObjects({
+            Bucket: "repository",
+            keys: [filename],
+        });
+    });
+    it("playing with writing out file chunks", async () => {
+        const file = "./src/config.js";
+        await remove("./src/test-config.js");
+        const fileSize = (await stat(file)).size;
+
+        let stream = createReadStream(file, { start: 0, end: 49 });
+        for await (let chunk of stream) {
+            await writeChunk({ chunk, position: 0 });
+        }
+        stream = createReadStream(file, { start: 50, end: 99 });
+        for await (let chunk of stream) {
+            await writeChunk({ chunk, position: 50 });
+        }
+        stream = createReadStream(file, { start: 100 });
+        for await (let chunk of stream) {
+            await writeChunk({ chunk, position: 100 });
+        }
     });
 });
+
+async function uploadFile({ file, filename, chunkSize = 100 }) {
+    // const file = "./src/config.js";
+    filename = new Buffer.from(filename).toString("base64");
+    const bucket = new Buffer.from("repository").toString("base64");
+    let fileSize = (await stat(file)).size;
+
+    // create the file
+    let response = await fetch("http://localhost:8080/files", {
+        method: "POST",
+        headers: {
+            "content-type": "application/offset+octet-stream",
+            "upload-length": fileSize,
+            "upload-metadata": `filename ${filename}, bucket ${bucket}, overwrite`,
+        },
+    });
+    expect(response.status).toEqual(201);
+    // console.log("post request", response.status);
+    const location = response.headers.get("location");
+
+    let moreToGo = true;
+    let start = 0;
+
+    let i = 0;
+    while (moreToGo) {
+        start = i * chunkSize;
+        let end = start + chunkSize - 1;
+        if (end > fileSize) end = fileSize;
+        // console.log("*****", start, end, fileSize, moreToGo);
+        let stream = createReadStream(file, { start, end, highWaterMark: 10 * 1024 * 1024 });
+        for await (let chunk of stream) {
+            let response = await fetch(location, {
+                method: "PATCH",
+                headers: {
+                    "content-type": "application/offset+octet-stream",
+                    "content-length": chunk.length,
+                    "upload-offset": start,
+                    "tus-resumable": "1.0.0",
+                },
+                body: chunk,
+            });
+            expect(response.status).toEqual(204);
+            // console.log(start, chunkSize, response.headers);
+            if (end < fileSize) {
+                // expect(response.headers.get("upload-offset")).toEqual(
+                //     String(start + chunkSize ?? end)
+                // );
+            } else {
+                expect(response.headers.get("upload-offset")).toEqual(String(end));
+            }
+            // console.log("patch request", response.status);
+        }
+        if (end >= fileSize) {
+            moreToGo = false;
+        }
+        i += 1;
+    }
+}
+
+async function writeChunk({ chunk, position }) {
+    const filename = "./src/test-config.js";
+    let fd = await open(filename, "a+");
+    await write(fd, chunk, null, null, position);
+    await close(fd);
+}
