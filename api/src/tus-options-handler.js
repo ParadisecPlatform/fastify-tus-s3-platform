823e9235fa3fbd1f835a30addfd44f2719bb3dc8
Marco La Rosa <m@lr.id.au>
2023-02-18 21:00:41 +1100

    refactor

--- api/src/tus-options-handler.js
+++ api/src/tus-options-handler.js
@@ -1,11 +1,10 @@
 import { maximumFileSize, tusExtensions } from "./config.js";
 export async function tusOptionsHandler(req, res) {
-    res.code(204)
-        .headers({
-            "Tus-Resumable": "1.0.0",
-            "Tus-Version": "1.0.0",
-            "Tus-Max-Size": maximumFileSize,
-            "Tus-Extension": tusExtensions,
-        })
-        .send();
+    const headers = {
+        "Tus-Resumable": "1.0.0",
+        "Tus-Version": "1.0.0",
+        "Tus-Max-Size": maximumFileSize,
+        "Tus-Extension": tusExtensions,
+    };
+    res.code(204).headers(headers).send();
 }
